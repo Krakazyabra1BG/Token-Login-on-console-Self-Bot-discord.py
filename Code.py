aa0b9a125b7087fa6fa812048b9e6cb56c480e79
Krakazyabra1BG <94980698+Krakazyabra1BG@users.noreply.github.com>
2021-11-25 04:10:59 +0300

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,8 @@
+```
+token = input('Enter your token ...')
+
+client.run(token)
+```
+Will now be automatically entered into
+```client.run```- will be considered as the authorization token of your 
+Account
